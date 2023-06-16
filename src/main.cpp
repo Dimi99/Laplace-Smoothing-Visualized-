0bf015ecc3438d38d8310cc7a95c58aa532de945
Dimi <dimitrios_K@gmx.de>
2023-06-16 10:45:57 +0200

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <OpenMesh/Core/Mesh/TriMeshT.hh>
-#include "OpenMesh/Core/IO/MeshIO.hh"
 #include "OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh"
 #include "mesh.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "util.h"
 #include "camera.h"
+
 
 #define _USE_MATH_DEFINES
 typedef OpenMesh::TriMesh_ArrayKernelT<>  MyMesh;
@@ -26,20 +25,19 @@ float lastFrame = 0.0f;
 auto fpsCount = 0;
 auto m_lastFPSDisplayTimestamp = glfwGetTime();
 
+glm::vec3 lightPos(0.8f, 1.0f, 0.9f);
+
 
 
 
 int main(int argc, char** argv) {
     std::string path = argc > 1 ? argv[1] : "bunny.obj";
-
-    // glfw: initialize and configure
-    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(SRC_WIDTH, SRC_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SRC_WIDTH, SRC_HEIGHT, "Laplacian Smoothing", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -48,24 +46,13 @@ int main(int argc, char** argv) {
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-
-
-    // tell GLFW to capture our mouse
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
-
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 #if(WIN32)
     Shader faceShader("../../../src/shader.vs", "../../../src/shader.fs");
@@ -73,116 +60,53 @@ int main(int argc, char** argv) {
 #else   
     Shader faceShader("../src/shader.vs", "../src/shader.fs");
     Shader edgeShader("../src/edge_shader.vs", "../src/edge_shader.fs");
-
 #endif
 
-
-
-    MyMesh  mesh;
-    if ( ! OpenMesh::IO::read_mesh(mesh,path))
-    {
-        std::cerr << "Error loading mesh from file " << path << std::endl;
-        return 1;
-    }
-    auto points = OpenMesh::getPointsProperty(mesh);
-    //auto faces = OpenMesh::makePropertyManagerFromExistingOrNew(mesh,"face",)
-
-    std::vector<Vertex> vertices;
-    std::vector<unsigned int> edges;
-    std::vector<unsigned int> indices;
-    mesh.request_face_normals();  // Request vertex normals
-    mesh.update_face_normals();
-    mesh.request_vertex_normals();  // Request vertex normals
-    mesh.update_vertex_normals();
-
-
-    for(auto vh: mesh.vertices()){
-        Vertex vertex{};
-        glm::vec3 vector;
-        // positions
-        vector.x = points(vh)[0];
-        vector.y = points(vh)[1];
-        vector.z = points(vh)[2];
-        vertex.Position = vector;
-        vertex.Normal = glm::vec3(mesh.normal(vh)[0],mesh.normal(vh)[1],mesh.normal(vh)[2]);
-        vertices.push_back(vertex);
-    }
-
-    indices.reserve(mesh.n_faces() * 3);
-    for (MyMesh::FaceIter f_it=mesh.faces_begin(); f_it!=mesh.faces_end(); ++f_it)
-    {
-        for (MyMesh::FaceVertexIter vv_it=mesh.fv_iter(*f_it); vv_it.is_valid(); ++vv_it)
-            indices.push_back(vv_it->idx());
-    }
-    edges.reserve(mesh.n_edges()*3);
-    // Traverse all edges in the mesh
-    for (MyMesh ::EdgeIter edge_it = mesh.edges_begin(); edge_it != mesh.edges_end(); ++edge_it)
-    {
-        // Get one of the halfedges of the current edge
-        MyMesh::HalfedgeHandle halfedge_handle = mesh.halfedge_handle(*edge_it, 0);
-
-        // Get the vertex indices of the halfedge endpoints
-        unsigned vertexIndex1 = mesh.to_vertex_handle(halfedge_handle).idx();
-        //unsigned vertexIndex2 = mesh.to_vertex_handle(mesh.next_halfedge_handle(halfedge_handle)).idx();
-        unsigned vertexIndex2 = mesh.from_vertex_handle(halfedge_handle).idx(); 
-
-        edges.push_back(vertexIndex1);
-        edges.push_back(vertexIndex2);
-    }
-
-
-    Mesh obj(vertices,indices,edges);
+    Mesh obj = model_to_mesh(path);
     int i = 0;
     int j = 0;
     while (!glfwWindowShouldClose(window))
     {
         auto m_currentFrameTimestamp = glfwGetTime();
 
-        if ((m_currentFrameTimestamp - m_lastFPSDisplayTimestamp) >= 1) {
+        if ((m_currentFrameTimestamp - m_lastFPSDisplayTimestamp) >= 0.00001) {
             obj.update_buffer();
-            obj.vertices.at(i).edit = 0.0f;
-            obj.vertices.at(++i).edit = 1.0f;
+            //obj.vertices.at(i).edit = 0.0f;
+            //obj.vertices.at(++i).edit = 1.0f;
             m_lastFPSDisplayTimestamp = m_currentFrameTimestamp;
 
         }
 
-
-
-
-        // per-frame time logic
-        // --------------------
         float currentFrame = static_cast<float>(glfwGetTime());
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
-
-        // input
-        // -----
         processInput(window,camera,deltaTime);
 
         // render
-        // ------
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         faceShader.use();
+        faceShader.setVec3("lightPos", lightPos);
+        faceShader.setVec3("viewPos", camera.Position);
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SRC_WIDTH / (float)SRC_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
         faceShader.setMat4("projection", projection);
         faceShader.setMat4("view", view);
 
 
-
         // render the loaded model
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f,  -2.5f, 0.0f));
-        model = glm::scale(model, glm::vec3(20.0f, 20.0f, 20.0f));
+        model = glm::translate(model, glm::vec3(0.0f,  -1.0f, 0.0f));
+        model = glm::rotate(model, (float)glfwGetTime()/2, glm::vec3(0.0f, 1.0f, 0.0f)); // where x, y, z is axis of rotation (e.g. 0 1 0)
+        model = glm::scale(model, glm::vec3(10.0f, 10.0f, 10.0f));
         faceShader.setMat4("model", model);
         glEnable(GL_POLYGON_OFFSET_FILL);
-        glPolygonOffset(1.0, 1.0); ///< may need adjustment for your use case
+        glPolygonOffset(1.0, 1.0);
         glDepthFunc(GL_LESS);
         obj.draw_faces(faceShader);
 
-
+        // render the mesh skeleton
         edgeShader.use();
         edgeShader.setMat4("projection", projection);
         edgeShader.setMat4("view", view);
@@ -192,16 +116,10 @@ int main(int argc, char** argv) {
         glDepthFunc(GL_LEQUAL);
         obj.draw_edges(edgeShader);
 
-
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
--- src/mesh.h
+++ src/mesh.h
@@ -1,7 +1,11 @@
 #ifndef MESH_H
 #define MESH_H
 
-#include "glad/glad.h" // holds all OpenGL type declarations
+#include "glad/glad.h"
+#include <OpenMesh/Core/Mesh/TriMeshT.hh>
+#include "OpenMesh/Core/IO/MeshIO.hh"
+#include "OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh"
+typedef OpenMesh::TriMesh_ArrayKernelT<>  MyMesh;
 
 
 
@@ -24,30 +28,19 @@
 #define MAX_BONE_INFLUENCE 200
 
 struct Vertex {
-    // position
     glm::vec3 Position;
-    // normal
     glm::vec3 Normal;
     float edit = 0.0f;
-    glm::vec4 paint = glm::vec4(1.0f,1.0f,1.0f,1.0f);
 
-    //bone indexes which will influence this vertex
-    int m_BoneIDs[MAX_BONE_INFLUENCE];
-    //weights from each bone
-    float m_Weights[MAX_BONE_INFLUENCE];
 };
 
 class Mesh {
 public:
-    // mesh Data
     std::vector<Vertex>       vertices;
     std::vector<unsigned int> indices;
     std::vector<unsigned int> edges;
-
     unsigned int VAO{};
     unsigned int VAO_EDGE{};
-
-    // constructor
     Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<unsigned> edges)
     {
         this->vertices = std::move(vertices);
@@ -57,21 +50,15 @@ public:
         // now that we have all the required data, set the vertex buffers and its attribute pointers.
         setupMesh();
     }
-
-    // render the mesh
     void draw_faces(Shader &shader)
     {
-        // draw mesh
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
-
         glBindVertexArray(0);
-
         glActiveTexture(GL_TEXTURE0);
     }
     void draw_edges(Shader &shader)
     {
-
         glBindVertexArray(VAO_EDGE);
         glDrawElements(GL_LINES, static_cast<unsigned int>(edges.size()), GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
@@ -81,83 +68,55 @@ public:
     void update_buffer(){
 
         glBindVertexArray(VAO);
-        // load data into vertex buffers
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        // A great thing about structs is that their memory layout is sequential for all its items.
-        // The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
-        // again translates to 3/2 floats which translates to a byte array.
         glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
-
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
-        // vertex Positions
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-        // vertex normals
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
-        // edit bool  normals
         glEnableVertexAttribArray(2);
         glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, edit));
 
         glBindVertexArray(VAO_EDGE);
-        // load data into vertex buffers
         glBindBuffer(GL_ARRAY_BUFFER, VBO_EDGE);
-        // A great thing about structs is that their memory layout is sequential for all its items.
-        // The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
-        // again translates to 3/2 floats which translates to a byte array.
         glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
-
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_EDGE);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, edges.size() * sizeof(unsigned int), &edges[0], GL_STATIC_DRAW);
-        // vertex Positions
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-        // vertex normals
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
-        // edit bool  normals
         glEnableVertexAttribArray(2);
         glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, edit));
     }
 
 private:
-    // render data
     unsigned int VBO{}, EBO{};
     unsigned int VBO_EDGE{}, EBO_EDGE{};
 
 
-    // initializes all the buffer objects/arrays
     void setupMesh()
     {
-        // create buffers/arrays
         glGenVertexArrays(1, &VAO);
         glGenBuffers(1, &VBO);
         glGenBuffers(1, &EBO);
 
         glBindVertexArray(VAO);
-        // load data into vertex buffers
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        // A great thing about structs is that their memory layout is sequential for all its items.
-        // The effect is that we can simply pass a pointer to the struct and it translates perfectly to a glm::vec3/2 array which
-        // again translates to 3/2 floats which translates to a byte array.
         glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 
-        // set the vertex attribute pointers
-        // vertex Positions
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-        // vertex normals
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
-        // edit bool  normals
         glEnableVertexAttribArray(2);
         glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, edit));
 
-        // create buffers/arrays
         glGenVertexArrays(1, &VAO_EDGE);
         glGenBuffers(1, &VBO_EDGE);
         glGenBuffers(1, &EBO_EDGE);
@@ -171,17 +130,71 @@ private:
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_EDGE);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, edges.size() * sizeof(unsigned int), &edges[0], GL_STATIC_DRAW);
 
-        // set the vertex attribute pointers
-        // vertex Positions
+
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-        // vertex normals
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
-        // edit bool  normals
         glEnableVertexAttribArray(2);
         glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, edit));
 
     }
 };
+
+
+Mesh model_to_mesh(const std::string& path){
+    MyMesh  mesh;
+    if ( ! OpenMesh::IO::read_mesh(mesh,path))
+    {
+        std::cerr << "Error loading mesh from file " << path << std::endl;
+    }
+    auto points = OpenMesh::getPointsProperty(mesh);
+    //auto faces = OpenMesh::makePropertyManagerFromExistingOrNew(mesh,"face",)
+
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> edges;
+    std::vector<unsigned int> indices;
+    mesh.request_face_normals();  // Request vertex normals
+    mesh.update_face_normals();
+    mesh.request_vertex_normals();  // Request vertex normals
+    mesh.update_vertex_normals();
+
+
+    for(auto vh: mesh.vertices()){
+        Vertex vertex{};
+        glm::vec3 vector;
+        // positions
+        vector.x = points(vh)[0];
+        vector.y = points(vh)[1];
+        vector.z = points(vh)[2];
+        vertex.Position = vector;
+        vertex.Normal = glm::vec3(mesh.normal(vh)[0],mesh.normal(vh)[1],mesh.normal(vh)[2]);
+        vertices.push_back(vertex);
+    }
+
+    indices.reserve(mesh.n_faces() * 3);
+    for (MyMesh::FaceIter f_it=mesh.faces_begin(); f_it!=mesh.faces_end(); ++f_it)
+    {
+        for (MyMesh::FaceVertexIter vv_it=mesh.fv_iter(*f_it); vv_it.is_valid(); ++vv_it)
+            indices.push_back(vv_it->idx());
+    }
+    edges.reserve(mesh.n_edges()*3);
+    // Traverse all edges in the mesh
+    for (MyMesh ::EdgeIter edge_it = mesh.edges_begin(); edge_it != mesh.edges_end(); ++edge_it)
+    {
+        // Get one of the halfedges of the current edge
+        MyMesh::HalfedgeHandle halfedge_handle = mesh.halfedge_handle(*edge_it, 0);
+
+        // Get the vertex indices of the halfedge endpoints
+        unsigned vertexIndex1 = mesh.to_vertex_handle(halfedge_handle).idx();
+        //unsigned vertexIndex2 = mesh.to_vertex_handle(mesh.next_halfedge_handle(halfedge_handle)).idx();
+        unsigned vertexIndex2 = mesh.from_vertex_handle(halfedge_handle).idx();
+
+        edges.push_back(vertexIndex1);
+        edges.push_back(vertexIndex2);
+    }
+
+
+    return {vertices,indices,edges};
+}
 #endif
